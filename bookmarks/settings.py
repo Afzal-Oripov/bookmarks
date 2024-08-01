099f45ead61f25e8ef847ebd3eb57163f5862f87
Afzal <afzaloripov007@gmail.com>
2024-08-01 16:38:02 +0500

    add pipeline social auth

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -95,6 +95,7 @@ AUTHENTICATION_BACKENDS = [
  'django.contrib.auth.backends.ModelBackend',
  'account.authentication.EmailAuthBackend',
  'social_core.backends.google.GoogleOAuth2',
+ 'social_core.backends.github.GithubOAuth2',
 ]
 
 # Password validation
@@ -144,18 +145,34 @@ LOGIN_REDIRECT_URL = 'dashboard'
 LOGIN_URL = 'login'
 LOGOUT_URL = 'logout'
 
-from ton import *
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = email
-EMAIL_HOST_PASSWORD = pasword
+EMAIL_HOST_USER ='afzaloripov007@gmail.com'
+EMAIL_HOST_PASSWORD = 'vuym diwz ejjc pmeb'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
 
 SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = '614667053447-ltdclrh9obek21fdjhec7qjrqmu8qg9s.apps.googleusercontent.com' # ИД клиента Google
 SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET =  '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' # Секрет клиента Google
+
+SOCIAL_AUTH_GITHUB_KEY = 'Ov23li3lgjcbgiHVnIt9'
+SOCIAL_AUTH_GITHUB_SECRET = '3bad0f00ee994d9bc452d5c8e82dffe214588861'
+
+SOCIAL_AUTH_PIPELINE = [
+ 'social_core.pipeline.social_auth.social_details',
+ 'social_core.pipeline.social_auth.social_uid',
+ 'social_core.pipeline.social_auth.auth_allowed',
+ 'social_core.pipeline.social_auth.social_user',
+ 'social_core.pipeline.user.get_username',
+ 'social_core.pipeline.user.create_user',
+ 'account.authentication.create_profile',
+ 'social_core.pipeline.social_auth.associate_user',
+ 'social_core.pipeline.social_auth.load_extra_data',
+ 'social_core.pipeline.user.user_details',
+]
