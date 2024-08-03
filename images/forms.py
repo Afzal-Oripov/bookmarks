4d1bda38a1e68c0f7c2f2266e61e5e23fa0db2b1
Afzal <afzaloripov007@gmail.com>
2024-08-03 18:26:07 +0500

    image hunter

--- images/forms.py
+++ images/forms.py
@@ -4,40 +4,37 @@ from django.core.files.base import ContentFile
 from django.utils.text import slugify
 import requests
 
+
 class ImageCreateForm(forms.ModelForm):
     class Meta:
         model = Image
         fields = ['title', 'url', 'description']
         widgets = {
-            'url': forms.HiddenInput,  # Assuming you want to keep URL hidden in the form
+            'url': forms.HiddenInput,
         }
 
     def clean_url(self):
         url = self.cleaned_data['url']
         valid_extensions = ['jpg', 'jpeg', 'png']
-        extension = url.rsplit('.', 1)[-1].lower()  # Extract the file extension
-        
+        extension = url.rsplit('.', 1)[1].lower()
         if extension not in valid_extensions:
-            raise forms.ValidationError('The given URL does not match valid image extensions.')
-        
+            raise forms.ValidationError('The given URL does not ' \
+                                        'match valid image extensions.')
         return url
 
-    def save(self, force_insert=False, force_update=False, commit=True):
-        # Create an instance without committing to the database
+    def save(self, force_insert=False,
+                   force_update=False,
+                   commit=True):
         image = super().save(commit=False)
         image_url = self.cleaned_data['url']
         name = slugify(image.title)
-        extension = image_url.rsplit('.', 1)[-1].lower()
+        extension = image_url.rsplit('.', 1)[1].lower()
         image_name = f'{name}.{extension}'
-        
-        # Download the image from the URL and save it to the ImageField
+        # download image from the given URL
         response = requests.get(image_url)
-        response.raise_for_status()  # Ensure the request was successful
-        image_content = ContentFile(response.content)
-        image.image.save(image_name, image_content, save=False)
-        
-        # Save the instance to the database
+        image.image.save(image_name,
+                         ContentFile(response.content),
+                         save=False)
         if commit:
             image.save()
-        
         return image
--- images/models.py
+++ images/models.py
@@ -1,11 +1,10 @@
 from django.db import models
 from django.conf import settings
 from django.utils.text import slugify
+from django.urls import reverse
 
 class Image(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL,
-                             related_name='images_created',
-                             on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='images_created', on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     slug = models.SlugField(max_length=200, blank=True)
     url = models.URLField(max_length=2000)
@@ -14,6 +13,9 @@ class Image(models.Model):
     created = models.DateField(auto_now_add=True)
     users_like = models.ManyToManyField(settings.AUTH_USER_MODEL,
                                         related_name='images_liked', blank=True)
+    def get_absolute_url(self):
+        return reverse('images:detail', args=[self.id, 
+                                              self.slug])
 
     class Meta:
         indexes = [
